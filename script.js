7d5d11796725ab6a89ed4c1ab3ffabd6f1d708a8
AshutoshPal9161 <palashutosh9161@gmail.com>
2026-02-12 00:04:57 +0530

    difference-between-innerText-innerHTML-and-textContent-in-js

---
+++ script.js
@@ -0,0 +1,7 @@
+const allimages = document.querySelectorAll('img');
+
+allimages[0].src = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
+
+allimages[1].src = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
+
+allimages[2].src = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
