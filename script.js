58f2ae662e2dbb0af1a08d9d17e3547d0662b967
AshutoshPal9161 <palashutosh9161@gmail.com>
2026-02-12 13:13:58 +0530

    to check the differences of innerText-innerHTML-and-textContent-in-js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const allimages = document.querySelectorAll('img');
+// const allimages = document.querySelectorAll('img');
 
-allimages[0].src = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
+// allimages[0].src = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
 
-allimages[1].src = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
+// allimages[1].src = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
 
-allimages[2].src = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
+// allimages[2].src = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60';
+
+
+const h1 = document.querySelector('h1')
+const paragraph = document.querySelector('p')
+
+// console.log(paragraph.innerHTML);
+
+// paragraph.innerHTML = '<h4>Hiii</h4>'
+
+// console.log(paragraph.innerText);
+// paragraph.innerText = '<h4>Hiii</h4>'
+
+// console.log(paragraph.textContent);
+// paragraph.textContent = '<h4>Hiii</h4>'
